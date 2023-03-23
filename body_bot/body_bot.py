b24649097ef6558683d2999d9e1640d5041f24d2
Dini4ka <boss.karelov@gmail.com>
2023-03-23 21:19:06 +0300

    adding method for making comments

--- body_bot/body_bot.py
+++ body_bot/body_bot.py
@@ -1,3 +1,5 @@
+import time
+
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from config import *
@@ -22,7 +24,7 @@ class reddit_bot:
         sleep(4)
 
     def making_posts(self, title, post):
-        # Going to the post link
+        # Going to the making_posts_field link
         self.driver.get(CREATE_POST_LINK)
 
         # Putting title
@@ -34,6 +36,16 @@ class reddit_bot:
         # Clicking for post
         self.driver.find_element(By.CSS_SELECTOR, '._18Bo5Wuo3tMV-RDB8-kh8Z').click()
 
+    def comment(self, post_link, comment_text):
+        # Going to the post need comment
+        self.driver.get(post_link)
+
+        # Putting text of comment
+        self.driver.find_element(By.CSS_SELECTOR, '.notranslate').send_keys(comment_text)
+
+        # Press button for comment
+        self.driver.find_element(By.CSS_SELECTOR, '._22S4OsoDdOqiM-hPTeOURa').click()
+        time.sleep(2)
     def end(self):
         self.driver.close()
 
@@ -42,5 +54,6 @@ if __name__ == '__main__':
     bot = reddit_bot()
     bot.get_reddit()
     bot.auth()
-    bot.making_posts('Всем привет', 'Привет, ребята, я новенький! ')
+    #bot.making_posts('Всем привет', 'Привет, ребята, я новенький! ')
+    bot.comment('https://www.reddit.com/r/marvelmemes/comments/11ysmwq/pour_one_out_for_all_the_bbegs_of_the_world/', 'Holly shit! ')
     bot.end()
